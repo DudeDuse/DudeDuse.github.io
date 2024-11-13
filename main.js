b10bebd258c01b30498ad9bec67ec8684388ed37
Catherine Ong <azeliaong@gmail.com>
2024-11-14 03:25:11 +1100

    testing gltf loader

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+const loader = new GLTFLoader();
+loader.load( 'mugtest/24_11_13_10_49_07_785.glb', 
+	function ( gltf ) { 
+		scene.add( gltf.scene ); 
+	}, 
+	undefined, 
+	function ( error ) {
+		 console.error( error ); 
+		} 
+	);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
